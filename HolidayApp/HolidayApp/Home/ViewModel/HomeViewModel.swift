327d6fe9088bcab1b88e24908bedfc245ad15318
Felipe Augusto <augusto_felipe@icloud.com>
2023-08-11 12:39:39 -0300

    [UPDATE] viewmodel func created to populate pickerview

--- HolidayApp/HolidayApp/Home/ViewModel/HomeViewModel.swift
+++ HolidayApp/HolidayApp/Home/ViewModel/HomeViewModel.swift
@@ -14,6 +14,32 @@ protocol HomeViewModelProtocol: AnyObject {
 
 class HomeViewModel {
     
-    private var holidayList: [Holiday]
+    private var holidayList: [Holiday] = []
     
+    var delegate: HomeViewModelProtocol?
+    
+    public func delegate(delegate: HomeViewModelProtocol) {
+        self.delegate = delegate
+    }
+    
+    public func getHolidays() -> [Holiday] {
+        return holidayList
+    }
+    
+    public var numberOfComponents: Int {
+        return 2
+    }
+    
+    public var numberOfRowsInComponent: Int {
+        return CountryData.countriesData.count
+    }
+    
+    public func titleForRow(pickerView: UIPickerView, row: Int, component: Int) -> String? {
+        if component == 0 {
+            let selectedCountryData = CountryData.countriesData[row]
+            return selectedCountryData["name"]
+        } else {
+            return CountryData.years[row]
+        }
+    }
 }
